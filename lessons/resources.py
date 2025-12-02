f80186fdf1eb678187671618b15e7c586b190c12
Ajit <ajit.megaproject@gmail.com>
2025-12-02 14:48:59 +0900

    Added resources for 1-20 lessons and modified upload script to upload the resources, and added shell script to sync the dev github from ajitkumar2026 to prod github urwithajit9

---
+++ lessons/resources.py
@@ -0,0 +1,184 @@
+RESOURCES = [
+    # Lesson 1
+    {
+        "lesson_day_index": 1,
+        "title": "Rust Official Documentation",
+        "url": "https://www.rust-lang.org/learn",
+        "image_url": "https://www.rust-lang.org/logos/rust-logo-512x512.png",
+    },
+    {
+        "lesson_day_index": 1,
+        "title": "Intro to Rust (YouTube)",
+        "url": "https://www.youtube.com/watch?v=zF34dRivLOw",
+        "image_url": "",
+    },
+    # Lesson 2
+    {
+        "lesson_day_index": 2,
+        "title": "Rust Variables and Mutability Guide",
+        "url": "https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html",
+        "image_url": "",
+    },
+    {
+        "lesson_day_index": 2,
+        "title": "Variables in Rust Explained (YouTube)",
+        "url": "https://www.youtube.com/watch?v=7uIRtR-KC6k",
+        "image_url": "",
+    },
+    # Lesson 3
+    {
+        "lesson_day_index": 3,
+        "title": "Rust Data Types Overview",
+        "url": "https://doc.rust-lang.org/book/ch03-02-data-types.html",
+        "image_url": "",
+    },
+    {
+        "lesson_day_index": 3,
+        "title": "Understanding Rust Types (Video)",
+        "url": "https://www.youtube.com/watch?v=Ulp1Kimblg0",
+        "image_url": "",
+    },
+    # Lesson 4
+    {
+        "lesson_day_index": 4,
+        "title": "Rust Functions Explained",
+        "url": "https://doc.rust-lang.org/book/ch03-03-how-functions-work.html",
+        "image_url": "",
+    },
+    {
+        "lesson_day_index": 4,
+        "title": "Rust Functions Tutorial (YouTube)",
+        "url": "https://www.youtube.com/watch?v=ygL_xcavzQ4",
+        "image_url": "",
+    },
+    # Lesson 5
+    {
+        "lesson_day_index": 5,
+        "title": "Rust Control Flow",
+        "url": "https://doc.rust-lang.org/book/ch03-05-control-flow.html",
+        "image_url": "",
+    },
+    {
+        "lesson_day_index": 5,
+        "title": "Control Flow in Rust (YouTube)",
+        "url": "https://www.youtube.com/watch?v=MsocPEZBd-M",
+        "image_url": "",
+    },
+    # Lesson 6
+    {
+        "lesson_day_index": 6,
+        "title": "Understanding Rust Data Types",
+        "url": "https://doc.rust-lang.org/book/ch03-02-data-types.html",
+        "image_url": "",
+    },
+    # Lesson 7
+    {
+        "lesson_day_index": 7,
+        "title": "Rust Functions and Methods",
+        "url": "https://doc.rust-lang.org/book/ch03-03-how-functions-work.html",
+        "image_url": "",
+    },
+    # Lesson 8
+    {
+        "lesson_day_index": 8,
+        "title": "Rust Ownership Basics",
+        "url": "https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html",
+        "image_url": "",
+    },
+    {
+        "lesson_day_index": 8,
+        "title": "Ownership in Rust Explained (YouTube)",
+        "url": "https://www.youtube.com/watch?v=8fFryjFzFoc",
+        "image_url": "",
+    },
+    # Lesson 9
+    {
+        "lesson_day_index": 9,
+        "title": "Borrowing and References",
+        "url": "https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html",
+        "image_url": "",
+    },
+    {
+        "lesson_day_index": 9,
+        "title": "Rust Borrowing Tutorial (YouTube)",
+        "url": "https://www.youtube.com/watch?v=G1Dq49kzS9Y",
+        "image_url": "",
+    },
+    # Lesson 10
+    {
+        "lesson_day_index": 10,
+        "title": "Conditional Statements in Rust",
+        "url": "https://doc.rust-lang.org/book/ch03-05-control-flow.html#if-expressions",
+        "image_url": "",
+    },
+    # Lesson 11
+    {
+        "lesson_day_index": 11,
+        "title": "Ownership Deep Dive",
+        "url": "https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html",
+        "image_url": "",
+    },
+    # Lesson 12
+    {
+        "lesson_day_index": 12,
+        "title": "Borrowing with References",
+        "url": "https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html",
+        "image_url": "",
+    },
+    # Lesson 13
+    {
+        "lesson_day_index": 13,
+        "title": "Lifetimes in Rust",
+        "url": "https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html",
+        "image_url": "",
+    },
+    # Lesson 14
+    {
+        "lesson_day_index": 14,
+        "title": "Writing Clean Functions",
+        "url": "https://doc.rust-lang.org/book/ch03-03-how-functions-work.html",
+        "image_url": "",
+    },
+    # Lesson 15
+    {
+        "lesson_day_index": 15,
+        "title": "Rust Modules",
+        "url": "https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html",
+        "image_url": "",
+    },
+    # Lesson 16
+    {
+        "lesson_day_index": 16,
+        "title": "Using the Rust Standard Library",
+        "url": "https://doc.rust-lang.org/std/",
+        "image_url": "",
+    },
+    # Lesson 17
+    {
+        "lesson_day_index": 17,
+        "title": "Working with Strings",
+        "url": "https://doc.rust-lang.org/book/ch08-02-strings.html",
+        "image_url": "",
+    },
+    # Lesson 18
+    {
+        "lesson_day_index": 18,
+        "title": "Tuples in Rust",
+        "url": "https://doc.rust-lang.org/book/ch03-02-data-types.html#compound-types",
+        "image_url": "",
+    },
+    # Lesson 19
+    {
+        "lesson_day_index": 19,
+        "title": "Structs in Rust",
+        "url": "https://doc.rust-lang.org/book/ch05-01-defining-structs.html",
+        "image_url": "",
+    },
+    # Lesson 20
+    {
+        "lesson_day_index": 20,
+        "title": "Enums in Rust",
+        "url": "https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html",
+        "image_url": "",
+    },
+]
